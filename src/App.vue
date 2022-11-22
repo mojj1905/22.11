3250165061f9019c46884ff3f5067c4103179c36
Mojj <109648273+mojj1905@users.noreply.github.com>
2022-11-22 22:58:28 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import HeaderVue from './components/Header.vue';
+import axios from 'axios'
+import BodyVue from './components/Body.vue';
+</script>
+<template>
+  <div>
+    <HeaderVue/>
+    <BodyVue :producs="producs"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      producs : []
+    }
+  },
+  created(){
+    this.axios.get('https://fakestoreapi.com/products?limit=3').then(({data})=>{
+      this.producs=data
+     
+
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Body.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <h1 class="text-center mt-3">Body</h1>
+    <div class="row">
+      <div class="col-4" v-for="item in 3" :key="item">
+        <div class="card my-3" style="width: 18rem">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              {{star.description}}
+              
+            </p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+        star : {}
+        
+    }
+    console.log(star);
+  },
+  props : {
+    producs : {
+        type : Array
+    }
+  },
+  
+}
+</script>
+
+<style scoped>
+</style>
